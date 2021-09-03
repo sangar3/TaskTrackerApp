58e69ac8ef40127a51abf341d7d157614c890d86
sangar3 <55110746+sangar3@users.noreply.github.com>
2021-09-03 11:55:29 -0500

    added toggle reminder with styles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -31,11 +31,20 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id)) //taking the tasks that are present, for each task that is not equal to the id will be removed
   }
 
+  // Toggle Reminder
+  const toggleReminder = (id) =>{
+    setTasks(
+      tasks.map((task) =>
+         task.id === id ? {...task, reminder:
+         !task.reminder} : task 
+        )
+    )
+  }
+
   return (
     <div className="container">
       <Header/>
-      
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'No Tasks Available'} 
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks Available'} 
     </div>
   );
 }
