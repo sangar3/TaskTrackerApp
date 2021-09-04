790a800167ff4cadfca96cc0b2b5929d4d741db2
sangar3 <55110746+sangar3@users.noreply.github.com>
2021-09-04 10:18:49 -0500

    ready for build

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -7,7 +7,11 @@ const Header = ({ title , onAdd, showAdd}) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button  color={showAdd ? '#db3a34' : '#457b9d' }  text={showAdd ? 'Close' : 'Add' } onClick ={onAdd}/>
+            <Button  
+                color={showAdd ? '#db3a34' : '#457b9d' } 
+                text={showAdd ? 'Close' : 'Add' } 
+                onClick ={onAdd}
+            />
         </header>
     )
 }
