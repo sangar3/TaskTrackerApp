5a067d410f69564a45d3c2deb9f55179bbd5e858
sangar3 <55110746+sangar3@users.noreply.github.com>
2021-09-04 12:26:25 -0500

    Add Task now Posts on server

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -29,10 +29,23 @@ function App() {
   }
 
   // Add Tasks
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 10000) + 1 // this gets a random id number 
-    const newTask = {id, ...task}
-    setTasks([...tasks,newTask])
+  const addTask = async  (task) => {
+  //   const id = Math.floor(Math.random() * 10000) + 1 // this gets a random id number 
+  //   const newTask = {id, ...task}
+  //   setTasks([...tasks,newTask])
+    
+    const res = await fetch('http://localhost:5000/tasks',{
+      method:'POST',
+      headers:{
+        'Content-type':'application/json'
+      },
+      body:JSON.stringify(task)
+    })
+
+    const data =  await res.json()
+
+    setTasks([...tasks,data])
+
   }
 
 
